ac7e89c7170611500687d76c13ef384382b9f97d
GonzaMaldonado <nahuel.maldonado.gonzalo@gmail.com>
2023-08-01 21:19:04 -0300

    feat: añadiendo autenticación de usuarios

--- src/api/users.ts
+++ src/api/users.ts
@@ -3,3 +3,8 @@ import { axi } from "./useAxios"
 export const registerRequest = async (username:string, email:string, first_name:string, last_name: string, password:string, confirm_password:string) => {
   await axi.post("/users/register/", {username, email, first_name, last_name, password, confirm_password})
 }
+
+export const loginRequest = async (username:string, password:string) => {
+  const response = await axi.post("/users/login/", {username, password})
+  return response;
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,77 @@
+import React, { useState } from "react";
+import { Link, useNavigate, Navigate } from "react-router-dom";
+import Logo from '../assets/cart.png';
+import { loginRequest } from "../api/users";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { useAuthStore } from "../store/auth";
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const { isAuth } = useAuthStore();
+  const setToken = useAuthStore((state) => state.setToken);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginMutation = useMutation({
+    mutationFn: () => loginRequest(username, password),
+    onSuccess: (res) => {
+      setToken(res.data.access, res.data.refresh);
+      toast.success("Login successfully");
+      navigate("/")
+    },
+    onError: (error) => {
+      toast.error("There was an error, please try to login again")
+      console.log(error);
+    }
+  })
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    loginMutation.mutate()
+  }
+
+  if (loginMutation.isLoading) return <p>Loading...</p>
+  if (isAuth) return (<Navigate to="/" />)
+
   return (
-    <div>Login</div>
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[800px] lg:py-0">
+    <Link to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      <img className="w-8 h-8 mr-2" src={Logo} alt="logo"/>
+      <span>Shop Zone</span>
+    </Link>
+    <div className="w-full md:w-[400px] lg:w-[500px] bg-slate-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Log in to Shop Zone
+        </h1>
+        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} >
+          <div>
+            <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
+            <input 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="username" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username"/>
+          </div>
+          
+          <div>
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <input 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+          </div>
+
+          <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Don't have an account? <Link to={'/register'} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
   )
 }
 
